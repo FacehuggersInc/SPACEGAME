23faa0a34fefee3a08f16ffc831b7a2550ef658b
FacehuggersInc <hulkchomper@gmail.com>
2023-02-25 23:18:08 -0600

    Added Variables

--- Engine/__init__.py
+++ Engine/__init__.py
@@ -1,7 +1,20 @@
 import os
-import pygame as engine
-from Engine.game import Game
+from random import randint
 
-os.system("CLS")
-print("CLIENT LOGS:")
+FPS = 165
+WIDTH = 1900#px
+HEIGHT = 1000#px
+
+#World Size
+wrld_size = 4
+wrld_width = WIDTH * wrld_size
+wrld_height = HEIGHT * wrld_size
+
+import pygame as engine
+entity = engine.sprite.Sprite
+img_load = engine.image.load
+refresh = engine.display.flip
+group = engine.sprite.Group
+
+from Engine.game import Game
 game = Game(engine)
