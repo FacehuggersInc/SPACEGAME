9b391378b3aa30945440fcafb6f6cfaed6fbb1ee
FacehuggersInc <hulkchomper@gmail.com>
2023-02-25 23:19:45 -0600

    Only need the functionality to quit the game.

--- Engine/InputManager.py
+++ Engine/InputManager.py
@@ -1,20 +1,9 @@
-class Input_Manager():
-    def __init__(self):
-        print("[InputManager] Instantiated")
-    
-    def manage(self, engine):
-        k = engine.key.get_pressed()
+from Engine import engine
 
+class Input_Manager():
+    def manage(self):
+        k = engine.key.get_pressed()
         #Events
         for event in engine.event.get():
             #Quit
             if event.type == engine.QUIT or k[engine.K_ESCAPE]: exit()
-
-            #Key Report
-            if event.type == engine.KEYDOWN:
-                key = str(event)
-                key = key[20:]
-                key = key.split(",")
-                key = key[0].split(":")
-                key = key[1].replace(" ","")
-                print(f"[InputManager]: [{key}] pressed.")
